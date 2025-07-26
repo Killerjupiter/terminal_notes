c47a15f4f1ceb9ab4f08040b2287986614e532c4
killerjupiter <alexrobe912@gmail.com>
2025-07-26 09:50:33 -0500

    added help function and updated dictionary for keywords

--- note_taker/note.py
+++ note_taker/note.py
@@ -1,4 +1,5 @@
 #By Bealex my discord is bealex if you need me
+
 #moves characters written in terminal into folder
 def write(key): 
     with open("note.txt", "a") as f:
@@ -24,7 +25,7 @@ def escape():
 def pass_on():
     pass
 
-#to change a line in the saved text file not working
+#to change a line in the saved text file
 def over_write():
     with open("note.txt", "r") as f:
         lines = f.readlines()
@@ -35,16 +36,54 @@ def over_write():
             for i in range(len(lines)):
                 f.write(lines[i])
 
+#help menu and all of it's options
+def help():
+    print("Type 'keys' for just the keywords, 'more' for all keyword info,")
+    print("or type the keyword you want more info about. Don't forget to type 'done' when done" )
+    print("")
+    keyword_list = list(keyword_dict.keys())
+    keyword_list.remove("")
+    while True:
+        key = input("input examples 'keys', 'done', or '/esc': ")
+        if key == "":
+            break
+        elif key == "keys":
+            print(keyword_list)
+        elif key in keyword_dict:
+            print(keyword_dict[key][0])
+        elif key == "more":
+            key = " "
+            for i in keyword_list:
+                print(i)
+                print(keyword_dict[i][0])
+                print("")
+        else:
+            break
+        
+            
+    
 
 #so keys don't get appended into file when typing and to call functions 
-keyword_dict = {"/esc": escape, "/read": read, "/clr": clear, "": pass_on, "/overwrite": over_write,}
+keyword_dict = {
+                "/esc": ["escape key exits program", escape], 
+                "/read": ["read key shows all saved text", read,], 
+                "/clr": ["clear key clears saved text", clear], 
+                "/overwrite": ["Replaces saved text at line indicated", over_write], 
+                "/help": ["shows information regarding keys", help], 
+                "": ["used for programming end user need not to worry about", pass_on],
+                }
 
 #where everything comes together
+print("/help is the keyword for help")
 while True:
     key = input()
 
     if key in keyword_dict:
-        keyword_dict[key]()
+        keyword_dict[key][1]()
+
+    elif key in keyword_dict and "1":
+        print("not added")
+        keyword_dict[key][2]()
 
     if key not in keyword_dict:
         write(key)
