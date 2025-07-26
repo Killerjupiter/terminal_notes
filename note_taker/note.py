a5624881038d06bfaa57b1bd7d1b95d1d8681325
killerjupiter <alexrobe912@gmail.com>
2025-07-26 05:05:05 -0500

    accidentally committed wrong version

--- note_taker/note.py
+++ note_taker/note.py
@@ -1,3 +1,4 @@
+#By Bealex my discord is bealex if you need me
 #moves characters written in terminal into folder
 def write(key): 
     with open("note.txt", "a") as f:
@@ -15,11 +16,16 @@ def clear():
     with open("note.txt", "w") as f:
         f.write("")
 
+#exits the program
 def escape():
     exit()
 
+#passes over so read doesn't save anything if you press enter and nothing else
+def pass_on():
+    pass
+
 #so keys don't get appended into file when typing and to call functions 
-keyword_dict = {"escape": escape, "read": read, "clear": clear,}
+keyword_dict = {"escape": escape, "read": read, "clear": clear, "": pass_on,}
 
 #where everything comes together
 while True:
